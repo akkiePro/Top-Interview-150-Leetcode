e989ebf0dc5900603b283bdff9b1c1a0c850e597
Shruti Prajapati <shrutzz1812@gmail.com>
2026-04-03 00:33:45 +0530

    Remove Duplicates II

--- src/main/RemoveDuplicatesII.java
+++ src/main/RemoveDuplicatesII.java
@@ -1,19 +1,19 @@
 package src.main;
 
 /**
- * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
  */
-public class RemoveDuplicates {
+public class RemoveDuplicatesII {
     public static void main(String[] args) {
-        int []nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {1,1,1,2,2,3}; // Output: 5, nums = [1,1,2,2,3,_]
         int newLength = removeDuplicates(nums);
         System.out.println("New length = " + newLength);
     }
 
     public static int removeDuplicates(int[] nums) {
-        int numsCounter = 1;
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i-1] != nums[i]) {
+        int numsCounter = 2;
+        for(int i=2; i<nums.length; i++) {
+            if(nums[numsCounter-2] != nums[i]) {
                 nums[numsCounter] = nums[i];
                 numsCounter++;
             }
